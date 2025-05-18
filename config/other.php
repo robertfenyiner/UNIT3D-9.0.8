d43c0e6d7867e309d017c4a705cfa89790b67944
Robert Niño <75451452+robertfenyiner@users.noreply.github.com>
2025-05-17 22:14:03 -0500

    Update other.php

--- config/other.php
+++ config/other.php
@@ -185,7 +185,7 @@ return [
     | True/1 = Enabled
     | False/0 = Disabled
     */
-    'application_signups' => false,
+    'application_signups' => true,
 
     /*
     |--------------------------------------------------------------------------
